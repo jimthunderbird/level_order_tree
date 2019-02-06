be9d31054b757804d36808416e256ce4ee0ce012
Jim Huang <jim.javathunderbird@gmail.com>
2019-02-06 11:34:53 -0800

    allow flexible callback functions as filters

--- 24.php
+++ 24.php
@@ -1,63 +1,99 @@
 <?php
-//0,1,2,3,4
-$numOfChilds = 13;
-$maxLevel = 4;
+class NestedLoop
+{
+    private $width;
+    private $depth;
+    private $filters;
+    private $paths;
 
-$traverseSequence = [[
-    'value' => 0,
-    'level' => 0,
-    'parent' => -1,
-    'path' => [],
-    'pathHash' => []
-]];
+    public function __construct($width, $depth, $filters) {
+        $this->width = $width;
+        $this->depth = $depth;
+        $this->filters = $filters;
+        $this->paths = [];
+        $this->compute();
+    }
 
-$parentPos = -1;
+    public function compute() {
+        $numOfChilds = $this->width;
+        $maxLevel = $this->depth;
 
-$result = [];
-while(true) {
-    $parentPos ++;
-    $curLevel = $traverseSequence[$parentPos]['level'] + 1;
-    if ($curLevel <= $maxLevel) {
-        for($i = 1; $i <= $numOfChilds; $i++) {
-            $pathHash = $traverseSequence[$parentPos]['pathHash'];
+        $traverseSequence = [[
+            'value' => 0,
+            'level' => 0,
+            'parent' => -1,
+            'path' => [],
+            'pathHash' => []
+        ]];
 
-            $filters = [
-                isset($pathHash[$i]),
-            ];
+        $parentPos = -1;
 
-            //filter, only find the unique sequence, ake, combination
-            $shouldSkip = false;
-            foreach($filters as $filter) {
-                if ($filter) {
-                    $shouldSkip = true;
-                    break;
+        while(true) {
+            $parentPos ++;
+            $curLevel = $traverseSequence[$parentPos]['level'] + 1;
+            if ($curLevel <= $maxLevel) {
+                for($i = 1; $i <= $numOfChilds; $i++) {
+                    $pathHash = $traverseSequence[$parentPos]['pathHash'];
+
+                    //we gather and current information and store it into a node
+                    $node = [];
+                    $node['value'] = $i;
+                    $node['level'] = $curLevel;
+                    $node['parentPathHash'] = $pathHash;
+
+                    //filter, only find the unique sequence, aka, combination
+                    $shouldSkip = false;
+                    foreach($this->filters as $filter) {
+                        if ($filter($node) === TRUE) {
+                            $shouldSkip = true;
+                            break;
+                        }
+                    }
+
+                    if ($shouldSkip) {
+                        continue;
+                    }
+
+                    $path = $traverseSequence[$parentPos]['path'];
+                    $path[] = $i;
+                    $pathHash[$i] = 1;
+                    $node['path'] = $path;
+                    $node['pathHash'] = $pathHash;
+                    $traverseSequence[] = $node;
                 }
+            } else {
+                break;
             }
-
-            if ($shouldSkip) {
-                continue;
-            }
-
-            $path = $traverseSequence[$parentPos]['path'];
-            $path[] = $i;
-            $pathHash[$i] = 1;
-            $traverseSequence[] = [
-                'value' => $i,
-                'parent' => $parentPos,
-                'level' => $curLevel,
-                'path' => $path,
-                'pathHash' => $pathHash
-            ];
         }
-    } else {
-        break;
+
+        foreach($traverseSequence as $node) {
+            if ($node['level'] === $maxLevel) {
+                $this->paths[] = $node['path'];
+            }
+        }
+    }
+
+    public function getPaths() {
+        return $this->paths;
     }
 }
 
-$counter = 0;
-foreach($traverseSequence as $node) {
-    if ($node['level'] === $maxLevel) {
-        $counter ++;
-        print implode(".", $node['path'])."\n";
+/**
+ * simulating:
+ * for ($i = 1; $i <= 4; $i++) {
+ *  for ($j = 1; $j <=4; $j++) {
+ *      for ($k = 1; $k <=4; $k++) {
+ *          for ($l = 1; $l <= 4; $l ++) {
+ *
+ *          }
+ *      }
+ *  }
+ * }
+ */
+$loop = new NestedLoop(4, 4,[
+    function($node) {
+        return isset($node['parentPathHash'][$node['value']]);
     }
-}
+]);
+
+print_r($loop->getPaths());
