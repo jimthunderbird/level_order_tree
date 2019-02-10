37a194b978d4d312592fd0d9c560aabd6c0a5f3c
Jim Huang <jim.javathunderbird@gmail.com>
2019-02-10 09:40:40 -0800

    as we walk down the word characters path, hash it

---
+++ libs/AutoCompleteDictionary2.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * The AutoComplete Dictionary
+ */
+class AutoCompleteDictionary2
+{
+    private $words;
+    private $levelHash;
+    private $maxLevel;
+
+    public function __construct() {
+        $this->wordPos = 0;
+        $this->words = [];
+        $this->levelHash = [];
+        $this->maxLevel = 0;
+    }
+
+    public function addWord($word) {
+        $length = strlen($word);
+
+        for ($k = 0; $k < $length; $k++) {
+            $level = $length - 1 - $k;
+            if ($k === 0) {
+                $pattern = $word;
+            } else {
+                $pattern = substr($word, 0, -$k);
+            }
+            $this->levelHash["$level$pattern"][] = $this->wordPos;
+        }
+
+        $this->words[] = $word;
+        $this->wordPos++;
+    }
+
+    public function getLevelHash() {
+        return $this->levelHash;
+    }
+
+    public function getMaxLevel() {
+        return $this->maxLevel;
+    }
+
+    public function search($prefix) {
+        $result = [];
+        $indexes = [];
+        $level = strlen($prefix) - 1;
+        if (isset($this->levelHash["$level$prefix"])) {
+            $indexes = $this->levelHash["$level$prefix"];
+        }
+        foreach($indexes as $index) {
+            $result[] = $this->words[$index];
+        }
+        return $result;
+    }
+}
+
+$dictionary = new AutoCompleteDictionary2();
+$words = [
+    'ape',
+    'ace',
+    'ame',
+    'apple',
+    'be',
+    'ben',
+    'build',
+    'bull',
+    'boll',
+    'ime',
+    'ice',
+    'box',
+    'bottle',
+    'fox',
+    'api',
+    'jim',
+    'jit',
+    'icecream',
+    'knot',
+    'knowledge',
+    'cool',
+    'application',
+    'apply'
+];
+foreach($words as $word) {
+    $dictionary->addWord($word);
+}
+print_r($dictionary->search('bu'));
