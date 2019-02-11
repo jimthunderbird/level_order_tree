a1527e7702820e7e29d114eee0ea9909bacb4e89
Jim Huang <jim.javathunderbird@gmail.com>
2019-02-11 08:45:56 -0800

    optimized key building

--- libs/AutoCompleteDictionary2.php
+++ libs/AutoCompleteDictionary2.php
@@ -13,12 +13,9 @@ class AutoCompleteDictionary2
     public function addWord($word) {
         $length = strlen($word);
 
+        $pattern = "";
         for ($k = 0; $k < $length; $k++) {
-            if ($k === 0) {
-                $pattern = $word;
-            } else {
-                $pattern = substr($word, 0, -$k);
-            }
+            $pattern .= $word[$k];
             $this->patternHash[$pattern][$word] = 1; //we do not want duplicates
         }
     }
